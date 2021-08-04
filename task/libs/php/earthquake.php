477f9b9180a85a4c268f0cf2500209c9b2fb6a89
Kizzy Sinar <Ranisimo@users.noreply.github.com>
2021-08-04 21:39:16 +0100

    Corrected mistake on line 27 'geonames' to 'earthquakes' Fixed 'undefined array key' error

--- task/libs/php/earthquake.php
+++ task/libs/php/earthquake.php
@@ -3,9 +3,11 @@
 	ini_set('display_errors', 'On');
 	error_reporting(E_ALL);
 
+    echo "Working?";
+
 	$executionStartTime = microtime(true);
 
-	$url='http://api.geonames.org/earthquakesJSON?formatted=true&north=' . $_REQUEST['north'] . '&south=' . $_REQUEST['south'] . '&east=' . $_REQUEST['east'] . '&west=' . $_REQUEST['west'] . '&username=kizzysinar&style=full';
+	$url='http://api.geonames.org/earthquakesJSON?&north=' . $_REQUEST['north'] . '&south=' . $_REQUEST['south'] . '&east=' . $_REQUEST['east'] . '&west=' . $_REQUEST['west'] . '&username=kizzysinar';
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -22,7 +24,7 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['geonames'];
+	$output['data'] = $decode['earthquakes'];
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
