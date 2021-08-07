3c2ae539819a5134bf81f26dd111799aa66cd05f
Kizzy Sinar <Ranisimo@users.noreply.github.com>
2021-08-07 14:42:31 +0100

    Updated url queries

--- task/libs/php/weather.php
+++ task/libs/php/weather.php
@@ -5,7 +5,7 @@
 
 	$executionStartTime = microtime(true);
 
-	$url='http://api.geonames.org/weatherJSON?formatted=true&north=' . $_REQUEST['north'] . '&south=' . $_REQUEST['south'] . '&east=' . $_REQUEST['east'] . '&west=' . $_REQUEST['west'] . '&username=kizzysinar&style=full';
+	$url='http://api.geonames.org/weatherJSON?&north=' . $_REQUEST['north'] . '&south=' . $_REQUEST['south'] . '&east=' . $_REQUEST['east'] . '&west=' . $_REQUEST['west'] . '&username=kizzysinar';
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -22,7 +22,7 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['geonames'];
+	$output['data'] = $decode['weatherObservations'];
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
