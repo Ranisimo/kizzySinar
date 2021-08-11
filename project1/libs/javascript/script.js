0c461f59033898ce0a796349c59372586cc3c405
Kizzy <66209284+Ranisimo@users.noreply.github.com>
2021-08-11 15:11:46 +0100

    Initial commit

---
+++ project1/libs/javascript/script.js
@@ -0,0 +1,11 @@
+$(window).on('load', function () {
+    if (
+        $('#preloader').length) {$('#preloader').delay(1000).fadeOut('slow',function () {
+            $(this).remove();
+        });
+    }
+});
+
+var mymap = L.map('mapid').setView([48.85661, 2.3515], 11);
+      L.tileLayer('https://tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png?access-token=nr1yo2LitcA3V1WiGnpQNQTE2ooNfxu7xxHlxldA7OirWsWMyBSLgoSzyVksXpdG', {}).addTo(mymap);
+      mymap.attributionControl.addAttribution("<a href=\"https://www.jawg.io\" target=\"_blank\">&copy; Jawg</a> - <a href=\"https://www.openstreetmap.org\" target=\"_blank\">&copy; OpenStreetMap</a>&nbsp;contributors")
