385eb9f12a73f6592dd7079d089b60aeafd53b30
Kizzy Sinar <Ranisimo@users.noreply.github.com>
2021-08-12 16:32:28 +0100

    Populating <select> query with JSON file Creating a PHP routine to access JSON content

--- project1/libs/javascript/script.js
+++ project1/libs/javascript/script.js
@@ -1,3 +1,4 @@
+console.log("script.js Loaded.")
 $(window).on('load', function () {
     if (
         $('#preloader').length) {$('#preloader').delay(1000).fadeOut('slow',function () {
@@ -6,6 +7,26 @@ $(window).on('load', function () {
     }
 });
 
+
 var mymap = L.map('mapid').setView([48.85661, 2.3515], 11);
       L.tileLayer('https://tile.jawg.io/jawg-light/{z}/{x}/{y}{r}.png?access-token=nr1yo2LitcA3V1WiGnpQNQTE2ooNfxu7xxHlxldA7OirWsWMyBSLgoSzyVksXpdG', {}).addTo(mymap);
-      mymap.attributionControl.addAttribution("<a href=\"https://www.jawg.io\" target=\"_blank\">&copy; Jawg</a> - <a href=\"https://www.openstreetmap.org\" target=\"_blank\">&copy; OpenStreetMap</a>&nbsp;contributors")
+      mymap.attributionControl.addAttribution("<a href=\"https://www.jawg.io\" target=\"_blank\">&copy; Jawg</a> - <a href=\"https://www.openstreetmap.org\" target=\"_blank\">&copy; OpenStreetMap</a>&nbsp;contributors");
+console.log("Leaflet Map Loaded.")
+
+
+$(document).ready(function() {
+    console.log("Select Function Loaded.");
+    $.ajax({
+      url: 'libs/php/getCountryISO.php',
+      type: 'POST',
+      dataType: 'json',
+      success: function(data) {
+          console.log(JSON.stringify(data));
+          console.log("JSON stringified");
+        //$.each(data, function(key, countryName) {
+            //var option = new Option(countryName, countryName);
+            //$(option).html(countryName);
+            //$("#countrySelect").append(option);
+        //})
+      }});
+  });
