d778343483bd883e7427a230ef00d6bf2b706166
Kizzy Sinar <Ranisimo@users.noreply.github.com>
2021-08-15 15:29:03 +0100

    Use PHP rountine to retrieve latlng

--- project1/libs/php/getCountryISO.php
+++ project1/libs/php/getCountryISO.php
@@ -5,7 +5,7 @@
 
 	$executionStartTime = microtime(true);
 
-	$url='project1\libs\json\countryBorders.geo.json';
+	$url='https://restcountries.eu/rest/v2/alpha/' . $_REQUEST['iso'];
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -22,7 +22,7 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['features'];
+	$output['data'] = $decode['latlng'];
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
