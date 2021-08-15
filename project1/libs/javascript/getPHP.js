b24fb46107d414eed26287c977f138fcc058d456
Kizzy Sinar <Ranisimo@users.noreply.github.com>
2021-08-15 15:28:33 +0100

    Added separate js file for PHP cURL routine

---
+++ project1/libs/javascript/getPHP.js
@@ -0,0 +1,28 @@
+console.log("getPHP loaded");
+$('#countrySearch').click(function() {
+    console.log("Button clicked");
+    $.ajax({
+        url: "libs/php/getCountryISO.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            iso: $('#countrySelect option:selected').val()
+        },
+        success: function(result) {
+
+            console.log(JSON.stringify(result));
+            console.log("JSON stringified");
+
+            if (result.status.name == "ok") {
+
+                console.log("All recieved well.");
+                $('mapid').setView(result['data']['latlng']);
+
+            }
+        
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
+        }
+    })
+});
