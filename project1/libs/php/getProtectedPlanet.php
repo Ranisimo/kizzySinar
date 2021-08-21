432c747c97076327c0e999f71fd82a121def38b4
Kizzy Sinar <Ranisimo@users.noreply.github.com>
2021-08-21 13:45:16 +0100

    tried different decode parameters, isn't working

--- project1/libs/php/getProtectedPlanet.php
+++ project1/libs/php/getProtectedPlanet.php
@@ -22,7 +22,7 @@
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['output'] = $decode['country'][0]['name'];
+	$output['output'] = $decode["country"]["name"];
 	
 	header('Content-Type: application/json; charset=UTF-8');
 
