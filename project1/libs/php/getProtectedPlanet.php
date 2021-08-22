84131582bf98bf8641ea146a3f0d947c9c9e8a0d
Kizzy Sinar <Ranisimo@users.noreply.github.com>
2021-08-23 00:27:03 +0100

    improved SSL security

--- project1/libs/php/getProtectedPlanet.php
+++ project1/libs/php/getProtectedPlanet.php
@@ -1,37 +1,101 @@
 <?php
+    function curl( $url=NULL, $options=NULL, $headers=false ){
+        /*
+            https://curl.haxx.se/docs/caextract.html
+            
+            # download own copy of cacert.pem from internet
+            # save in suitable location, edit below path.           
+        */
+        $cacert='C:\xampp\htdocs\project1\libs\php\cacert-2021-07-05.pem';
+        $vbh = fopen('php://temp', 'w+');
 
-	ini_set('display_errors', 'On');
-	error_reporting(E_ALL);
+        
+        $res=array(
+            'response'  =>  NULL,
+            'info'      =>  array( 'http_code' => 100 ),
+            'headers'   =>  NULL,
+            'errors'    =>  NULL
+        );
+        if( is_null( $url ) ) return (object)$res;
 
-	$executionStartTime = microtime(true);
+        session_write_close();
 
-	$url='http://api.protectedplanet.net/v3/countries/' . $_REQUEST['iso'] . '?token=40443092885a889142e0bce8b74975b3';
+        /* Initialise curl request object */
+        $curl=curl_init();
+        if( parse_url( $url,PHP_URL_SCHEME )=='https' ){
+            curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, true );
+            curl_setopt( $curl, CURLOPT_SSL_VERIFYHOST, 2 );
+            curl_setopt( $curl, CURLOPT_CAINFO, $cacert );
+            curl_setopt( $curl, CURLOPT_CAPATH, $cacert );
+        }
 
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($ch, CURLOPT_URL,$url);
+        /* Define standard options */
+        curl_setopt( $curl, CURLOPT_URL,trim( $url ) );
+        curl_setopt( $curl, CURLOPT_AUTOREFERER, true );
+        curl_setopt( $curl, CURLOPT_FOLLOWLOCATION, true );
+        curl_setopt( $curl, CURLOPT_FAILONERROR, true );
+        curl_setopt( $curl, CURLOPT_HEADER, false );
+        curl_setopt( $curl, CURLINFO_HEADER_OUT, false );
+        curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $curl, CURLOPT_CONNECTTIMEOUT, 20 );
+        curl_setopt( $curl, CURLOPT_TIMEOUT, 60 );
+        curl_setopt( $curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.38 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.38' );
+        curl_setopt( $curl, CURLOPT_MAXREDIRS, 10 );
+        curl_setopt( $curl, CURLOPT_ENCODING, '' );
+        
+        curl_setopt( $curl, CURLOPT_VERBOSE, true );
+        curl_setopt( $curl, CURLOPT_NOPROGRESS, true );
+        curl_setopt( $curl, CURLOPT_STDERR, $vbh );
+        
 
-	$result=curl_exec($ch);
+        /* Assign runtime parameters as options */
+        if( isset( $options ) && is_array( $options ) ){
+            foreach( $options as $param => $value ) curl_setopt( $curl, $param, $value );
+        }
+        
+        if( $headers && is_array( $headers ) ){
+            curl_setopt( $curl, CURLOPT_HTTPHEADER, $headers );
+        }
 
-	curl_close($ch);
-
-	$decode = json_decode($result,true);	
-
-	$output['status']['code'] = "200";
-	$output['status']['name'] = "ok";
-	$output['status']['description'] = "success";
-	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['landarea'] = $decode['country']['statistics']['land_area'];
-	$output['marinearea'] = $decode['country']['statistics']['marine_area'];
-	$output['palandarea'] = $decode['country']['statistics']['pa_land_area'];
-	$output['pamarinearea'] = $decode['country']['statistics']['pa_marine_area'];
-	$output['percentpalandarea'] = $decode['country']['statistics']['percentage_pa_land_cover'];
-	$output['percentpamarinearea'] = $decode['country']['statistics']['percentage_pa_marine_cover'];
-	
-	header('Content-Type: application/json; charset=UTF-8');
-
-	echo json_encode($output); 
-
+        /* Execute the request and store responses */
+        $res=(object)array(
+            'response'  =>  curl_exec( $curl ),
+            'info'      =>  (object)curl_getinfo( $curl ),
+            'errors'    =>  curl_error( $curl )
+        );
+        rewind( $vbh );
+        $res->verbose=stream_get_contents( $vbh );
+        fclose( $vbh );
+        curl_close( $curl );
+        return $res;
+    };
+?>
+<?php
+	$start=microtime( true );
+    $url=sprintf( 'https://api.protectedplanet.net/v3/countries/' . $_REQUEST['iso'] . '?token=40443092885a889142e0bce8b74975b3');
+    $res=curl( $url );
+    
+    if( $res->info->http_code==200 ){
+        $json=json_decode( $res->response );
+        $output=array(
+            'landarea'    =>  $json->country->statistics->land_area,
+			'marinearea'	=> $json->country->statistics->marine_area,
+			'palandarea'	=> $json->country->statistics->pa_land_area,
+			'pamarinearea'	=> $json->country->statistics->pa_marine_area,
+			'percentpalandarea'	=> $json->country->statistics->percentage_pa_land_cover,
+			'percentpamarinearea'	=> $json->country->statistics->percentage_pa_marine_cover,
+            'status'    =>  array(
+                'code'          =>  200,
+                'name'          =>  'ok',
+                'description'   =>  'success',
+                'returnedIn'    =>  intval( ( microtime( true ) - $start ) * 1000 ) . 'ms'
+            )
+        );
+    }else{
+        $output=$res;
+    }
+    
+    
+    header('Content-Type: application/json; charset=UTF-8');
+    exit( json_encode( $output ) );
 ?>
