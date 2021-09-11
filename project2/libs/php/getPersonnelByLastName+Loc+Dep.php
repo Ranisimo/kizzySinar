73ee865b9a4fe1010394b10ef1abc0694f2124fd
Kizzy Sinar <Ranisimo@users.noreply.github.com>
2021-09-11 22:45:09 +0100

    handles search parameters any name, first + last

--- project2/libs/php/getPersonnelByLastName+Loc+Dep.php
+++ project2/libs/php/getPersonnelByLastName+Loc+Dep.php
@@ -86,41 +86,11 @@
 
 	}
 
-	// second query - does not accept parameters and so is not prepared
-
-	$query = 'SELECT id, name from department ORDER BY name';
-
-	$result = $conn->query($query);
-	
-	if (!$result) {
-
-		$output['status']['code'] = "400";
-		$output['status']['name'] = "executed";
-		$output['status']['description'] = "query failed";	
-		$output['data'] = [];
-
-		mysqli_close($conn);
-
-		echo json_encode($output); 
-
-		exit;
-
-	}
-   
-   	$department = [];
-
-	while ($row = mysqli_fetch_assoc($result)) {
-
-		array_push($department, $row);
-
-	}
-
 	$output['status']['code'] = "200";
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
 	$output['data']['personnel'] = $personnel;
-	$output['data']['department'] = $department;
 	
 	mysqli_close($conn);
 
