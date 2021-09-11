e76c62c5a6f319b7d138fbf3fd07a7c8c1dd102e
Kizzy Sinar <Ranisimo@users.noreply.github.com>
2021-09-11 22:44:15 +0100

    added deletion php routines won't delete if entry has dependants

--- project2/libs/php/deleteLocationByID.php
+++ project2/libs/php/deleteLocationByID.php
@@ -36,9 +36,9 @@
 	// SQL statement accepts parameters and so is prepared to avoid SQL injection.
 	// $_REQUEST used for development / debugging. Remember to change to $_POST for production
 
-	$prequery = $conn->prepare('SELECT * FROM personnel WHERE departmentID = ?');
+	$prequery = $conn->prepare('SELECT * FROM department WHERE locationID = ?');
 	
-	$prequery->bind_param("i", $_POST['deleteDepartmentID']);
+	$prequery->bind_param("i", $_POST['deleteLocationID']);
 
 	$prequery->execute();
 
@@ -46,21 +46,21 @@
 	
 	if ($prequery->affected_rows === 0) {
 
-		$query = $conn->prepare('DELETE FROM department WHERE id = ?');
+        $query = $conn->prepare('DELETE FROM location WHERE id = ?');
 	
-		$query->bind_param("i", $_POST['deleteDepartmentID']);
+        $query->bind_param("i", $_REQUEST['deleteLocationID']);
 
-		$query->execute();
+        $query->execute();
 
 		$output['status']['code'] = "200";
 		$output['status']['name'] = "ok";
 		$output['status']['description'] = "success";
 		$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
 		$output['data'] = [];
-		
+
 		mysqli_close($conn);
 
-		echo json_encode($output);  
+		echo json_encode($output); 
 
 		exit;
 
